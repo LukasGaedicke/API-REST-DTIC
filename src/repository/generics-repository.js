629b0411ca7ed2e6da03bc53b4c59b56daf633e3
LukasGaedicke <lukasfgaedicke@gmail.com>
2019-05-10 14:50:09 -0300

    Generic verification in Mongo

--- src/repository/generics-repository.js
+++ src/repository/generics-repository.js
@@ -4,10 +4,9 @@
 const mongoose = require('mongoose');
 
 const Header = mongoose.model('Header', { name: String });
+const MENUS = mongoose.model('Menu', { name: String });
 //const Alunos = mongoose.model('Aluno', { name: String });
 const helper = require('../helper/montarJson');
-
-
 
 
 exports.getHeaderGenerics = async (data) => {
@@ -21,61 +20,49 @@ exports.getHeaderGenerics = async (data) => {
     default:
       throw new Error();
   }
-
-
 }
 
 exports.getDataGenerics = async (data, inicio, fim, search) => {
-  
   //QUANDO ELE CRIA A ENTIDADE, ELE COLOCA PARA PLURAL. EX-> TESTE fica TESTES, PLAY fica PLAYS
   var GenericModel = null;
-
-  try {
-    GenericModel = mongoose.model(data, { nome: String });
-  } catch (e) {
-    GenericModel = mongoose.model(data);
-  }
-  if (search != "") {
-    return await buscaComFiltro(GenericModel, inicio, fim, search);
+  if (await verificarExistenciaModel(data) != false) {
+    try {
+      GenericModel = mongoose.model(data);
+    } catch (e) {
+      GenericModel = mongoose.model(data, { nome: String });
+    }
+    if (search != "") {
+      return await buscaComFiltro(GenericModel, inicio, fim, search);
+    } else {
+      return await buscaSemFiltro(GenericModel, inicio, fim);
+    }  
   } else {
-    return await buscaSemFiltro(inicio, fim);
+    throw Error("Essa entidade não existe.");
   }
+  
 }
 
-async function buscaComFiltro(MODEL , inicio, fim, search) {
+async function buscaComFiltro(MODEL, inicio, fim, search) {
   var res = await MODEL.find({ 'nome': new RegExp(search, 'i') }, { __v: 0, _id: 0 }).skip(inicio).limit(fim);
   var totalF = await MODEL.find({ 'nome': new RegExp(search, 'i') }, { __v: 0, _id: 0 }).count();
   var total = await MODEL.find({}).count();
   var jsonMontado = helper.montarJson(res, total, totalF);
   return jsonMontado;
 }
-async function buscaSemFiltro(MODEL,inicio, fim) {
+async function buscaSemFiltro(MODEL, inicio, fim) {
   var res = await MODEL.find({}, { __v: 0, _id: 0 }).skip(inicio).limit(fim);
   var total = await MODEL.find({}).count();
   var jsonMontado = helper.montarJson(res, total, total);
   return jsonMontado;
 }
 
-/*
-exports.getDataGenerics = async (data, inicio, fim, search) => {
-  if (search != "") {
-    return await buscaComFiltro(inicio, fim, search);
-  } else {
-    return await buscaSemFiltro(inicio, fim);
-  }
-}
 
-async function buscaComFiltro(inicio, fim, search) {
-  var res = await Alunos.find({ 'nome': new RegExp(search, 'i') }, { __v: 0, _id: 0 }).skip(inicio).limit(fim);
-  var totalF = await Alunos.find({ 'nome': new RegExp(search, 'i') }, { __v: 0, _id: 0 }).count();
-  var total = await Alunos.find({}).count();
-  var jsonMontado = helper.montarJson(res, total, totalF);
-  return jsonMontado;
+async function verificarExistenciaModel(data) {
+  var res = await MENUS.find({ 'nome': data});
+  if (res != "") {
+    return true; 
+  } else {
+    return false; 
+  }
+
 }
-async function buscaSemFiltro(inicio, fim) {
-  var res = await Alunos.find({}, { __v: 0, _id: 0 }).skip(inicio).limit(fim);
-  var total = await Alunos.find({}).count();
-  var jsonMontado = helper.montarJson(res, total, total);
-  return jsonMontado;
-}
-*/
